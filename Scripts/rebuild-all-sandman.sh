2f8d1584f8bce27f6909831634da88fb1164a4b1
julioln <1333545+julioln@users.noreply.github.com>
2022-04-23 18:17:54 -0300

    Updates

--- Scripts/rebuild-all-sandman.sh
+++ Scripts/rebuild-all-sandman.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
+# Update base docker images
 docker_images=$(podman images | grep docker | cut -d ' ' -f 1)
-[ -n "$docker_images" ] && podman image pull $docker_images
+[ -n "$docker_images" ] && parallel --line-buffer -j '50%' podman image pull {} ::: $docker_images
 
+# Update base sandman builds
+base=$(find -L ~/.config/sandman/base -maxdepth 1 -name *.toml | xargs basename -a | cut -d. -f 1)
+[ -n "$conts" ] && parallel --line-buffer -j '50%' sandman build base/{} ::: $base
+
+# Update the sandman builds
 conts=$(find -L ~/.config/sandman -maxdepth 1 -name *.toml | xargs basename -a | cut -d. -f 1)
-for s in $conts; do 
-	sandman build $s &
-done
-
-wait
+[ -n "$conts" ] && parallel --line-buffer -j '50%' sandman build {} ::: $conts
 
 podman image prune -f
